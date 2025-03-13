591a5f8cbf426357b84b5d77d998ae80531c1276
b184896 <bredetorgersen@gmail.com>
2025-03-13 14:42:39 +0100

    Update Oppgave_4.java
    upgv 4

--- src/uke_11/Oppgave_4.java
+++ src/uke_11/Oppgave_4.java
@@ -1,5 +1,30 @@
 package uke_11;
 
+import java.util.*;
+
 public class Oppgave_4 {
+	public static void main(String args[]) {
+		
+		HashSet<Integer> hSet = new HashSet<>();
+		Integer[] tab = new Integer[100000];
+		
+		int tall = 376;
+		
+		for(int i = 0; i < 100000;) {
+			hSet.add(tall);
+			tab[i] = tall;
+			tall = (tall + 45713) % 1000000;
+		}
+		
+		Arrays.sort(tab);
+		
+		Random random = new Random();
+		int[] soketall = new int[10000];
+		for(int i = 0; i < 10000; i++) {
+			soketall[i] = random.nextInt(1000000);
+		}
+		
+		
+	}
 
 }
