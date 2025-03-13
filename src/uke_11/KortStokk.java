80f78916296a9bfcc5a2ab6bb69926170b1712f5
b184896 <bredetorgersen@gmail.com>
2025-03-13 15:46:53 +0100

    hguhuh

--- src/uke_11/KortStokk.java
+++ src/uke_11/KortStokk.java
@@ -1,19 +1,17 @@
 package uke_11;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 import uke_11.KortVerdi;
 import uke_11.KortFarge;
 import uke_11.Kort;
 
-public class KortStokk implements Iterable<Kort>{
+public class KortStokk {
 	
 	private LinkedList<Kort> kortstokk;
 	
-	public KortStokk(int antall) {
+	public void kortStokk(int antall) {
 		kortstokk = new LinkedList<>();
 		for(KortFarge farge : KortFarge.values()) {
 			for(KortVerdi verdi : KortVerdi.values()) {
@@ -31,34 +29,5 @@ public class KortStokk implements Iterable<Kort>{
 		} else {
 			throw new IllegalStateException("Kortstokken er tom");
 		}
-	}
-	//B)
-	@Override
-	public Iterator<Kort> iterator() {
-		return kortstokk.iterator();
-	}
-	public void skrivUt() {
-		for (Kort kort : this) {
-			System.out.println(kort);
-		}
-	}
-	
-	public static void main(String[] args) {
-		
-		KortStokk stokk = new KortStokk(52);
-		
-		System.out.println("Ustokket kortstokk: "); 
-		stokk.skrivUt();
-		
-		System.out.println("\nStokker kortstokken: ");
-		stokk.Shuffle();
-		
-		System.out.println("\nStokket kortstokk: ");
-		stokk.skrivUt();
-		
-		System.out.println("\nTrekket kort: ");
-		System.out.println(stokk.trekkKort());
-		
-		
 	}
 }
--- src/uke_11/Oppgave_4.java
+++ src/uke_11/Oppgave_4.java
@@ -10,7 +10,7 @@ public class Oppgave_4 {
 		
 		int tall = 376;
 		
-		for(int i = 0; i < 100000;) {
+		for(int i = 0; i < 100000; i++) {
 			hSet.add(tall);
 			tab[i] = tall;
 			tall = (tall + 45713) % 1000000;
@@ -19,11 +19,36 @@ public class Oppgave_4 {
 		Arrays.sort(tab);
 		
 		Random random = new Random();
+		
 		int[] soketall = new int[10000];
 		for(int i = 0; i < 10000; i++) {
 			soketall[i] = random.nextInt(1000000);
 		}
 		
+		long startTid = System.currentTimeMillis();
+		int funnetiHset = 0;
+		for(int tallsok : soketall) {
+			if(hSet.contains(tallsok)) {
+				funnetiHset++;
+			}
+		}
+		
+		long sluttTid = System.currentTimeMillis();
+		long tidHset = sluttTid - startTid;
+		
+		long startTid2 = System.currentTimeMillis();
+		int funnetiTab = 0;
+		for(int tallsok : soketall) {
+			if(Arrays.binarySearch(tab, tallsok) >= 0) {
+				funnetiTab++;
+			}
+		}
+		
+		long sluttTid2 = System.currentTimeMillis();
+		long tidTab = sluttTid2 - startTid2;
+		
+		System.out.println("Søking i HashSet: " + tidHset + " ms, funnet: " + funnetiHset);
+		System.out.println("Søking i Tabell: " + tidTab + " ms, funnet: " + funnetiTab);
 		
 	}
 
